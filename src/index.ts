58a17313c5c610bfe10aecc82a15d6c5b1ef63d6
Diego Lisboa Giglioli <d.lisboa.giglioli@avanade.com>
2023-06-19 19:33:51 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Posts } from './resources/posts';
+
+export class Library {
+  posts: Posts;
+
+  constructor(config: { apiKey: string; baseUrl?: string }) {
+    this.posts = new Posts(config);
+  }
+}
